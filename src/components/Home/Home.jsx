035f9e24f20804e7e1d514e78d884ba28720e5e4
Brian Caceres <briancaceres98@gmail.com>
2023-09-20 19:25:43 -0300

    más imagenes en home (mobile)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,38 @@
 import './Home.css';
 import Cabana1 from '../../assets/imgs/cabanasimgs/cabanas.jpg';
 import Cabana2 from '../../assets/imgs/cabanasimgs/variasCabanas.jpeg';
+import Cabana3 from '../../assets/imgs/cabanasimgs/cabana3.jpg';
+import Cabana4 from '../../assets/imgs/cabanasimgs/cabana4.jpg';
+import Cabana5 from '../../assets/imgs/cabanasimgs/cabana5.jpg';
+import Cabana6 from '../../assets/imgs/cabanasimgs/cabana6.jpg';
+import Cabana7 from '../../assets/imgs/cabanasimgs/cabana7.jpg';
+import Cabana8 from '../../assets/imgs/cabanasimgs/cabana8.png';
+import Cabana9 from '../../assets/imgs/cabanasimgs/cabana9.png';
+import Cabana10 from '../../assets/imgs/cabanasimgs/cabana10.png';
+import Cabana11 from '../../assets/imgs/cabanasimgs/cabana11.png';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
-      <h1 className='h1Home'>Cabañas</h1>
-      <h2 className='h2Home'>Subtitulo</h2>
+      <h1 className='h1Home'>Bienvenid@</h1>
+      <h2 className='h2Home'>Ahora puede elegir donde quiere pasar un tiempo de tranquilidad</h2>
 
       <div className='imgsContainer'>
         <img src={Cabana1} alt="cabaña1" />
         <img src={Cabana2} alt="cabaña2" />
+        <img src={Cabana3} alt="cabaña3" />
+        <img src={Cabana4} alt="cabaña4" />
+        <img src={Cabana5} alt="cabaña5" />
+        <img src={Cabana6} alt="cabaña6" />
+        <img src={Cabana7} alt="cabaña7" />
+        <img src={Cabana8} alt="cabaña8" />
+        <img src={Cabana9} alt="cabaña9" />
+        <img src={Cabana10} alt="cabaña10" />
+        <img src={Cabana11} alt="cabaña11" />
       </div>
+
+      <p>Si quiere conocer las promos haga click <Link className='links' to="/promos">aquí</Link></p>
     </>
   )
 }
--- src/components/cabanas/Cabanas.jsx
+++ src/components/cabanas/Cabanas.jsx
@@ -1,45 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Cabanas.css';
 
 function Cabanas() {
+
+
+  // useEffect(()=>{
+  //   fetch('./cabanas.json')
+  //   .then(res => res.json())
+  //   .then(res => console.log(res))
+  //   .catch(e => console.error(e))
+  // }, []) no funciona estoy viendo como hacer que funcione xD
+
   return (
     <>
-      <h2 className='cab'>Cabanas</h2>
-      <p className='pCabanas'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem possimus vitae quis assumenda tempora excepturi alias, sapiente commodi rerum, veniam quo doloremque adipisci, praesentium illum! Quae facilis voluptates deleniti error.
-      Fugit quo, incidunt exercitationem delectus dolorum saepe veniam officiis, accusamus odit nulla aspernatur nihil earum, eligendi soluta culpa unde fuga id ex perspiciatis atque ea. Aperiam magnam vitae atque non!
-      Maxime consequatur perspiciatis quae voluptatum earum libero numquam corrupti ea, eaque mollitia eveniet ipsam molestiae unde, dolore illo consectetur. Saepe aliquid dolorum ab provident minus dolores animi similique sed id.
-      Enim, nulla! Voluptatum distinctio sit recusandae, officia est consectetur enim, excepturi eum ipsum eos sequi perspiciatis deserunt nulla provident nesciunt optio non veniam ex voluptatibus. Rerum fugiat tempora iusto rem.
-      Accusantium magnam nulla quos! Excepturi, voluptate! Labore, impedit! Tempora eaque consequuntur vero eveniet! Ut explicabo quam corporis adipisci repudiandae dicta ipsum corrupti, accusamus nulla aperiam assumenda quos vero commodi autem.
-      Suscipit, unde accusamus consequatur quis quam at earum odit nisi corrupti similique, maiores ut dolorum fugiat exercitationem nihil labore enim nemo necessitatibus magni? Magnam consectetur totam natus cumque assumenda ducimus.
-      Nobis facilis ea quo ad vero sunt repudiandae debitis cumque aliquam quam est eos impedit itaque asperiores autem modi, repellat odio qui architecto quia laudantium reprehenderit. Minima, quisquam. Sunt, pariatur?
-      Mollitia natus dolorum voluptatibus, obcaecati quidem voluptatem, nulla assumenda dignissimos veniam vitae rem ad minima porro! Eum dicta ut id unde atque tempore? Deserunt suscipit sunt aspernatur molestias excepturi doloremque.
-      Dolore asperiores, quidem vitae debitis a vero sint id. Enim illum aspernatur quas! Magni, ipsum iste. Quod, odit repellat voluptates totam provident necessitatibus unde exercitationem, esse hic assumenda, amet cumque?
-      Nam, illum sit deleniti, dolore odit, est iusto vitae quae repellat facilis vel id temporibus nostrum. Laudantium quidem incidunt facere hic ut doloremque ducimus, iusto error molestiae minima autem vel?
-      Vel, voluptate. Esse, deserunt. Illum, ut distinctio! Eum recusandae dolorum non, officia excepturi dolorem autem! Voluptatum quo harum tempore repudiandae quam at optio aspernatur, provident, corporis officiis, eum totam est.
-      Soluta aspernatur iste unde id aut? Ut alias explicabo corrupti facere sint necessitatibus nemo qui recusandae, eaque doloribus inventore vitae earum deleniti magni, aliquam amet omnis quidem aut assumenda eius!
-      Repellendus illo ratione aliquid! Pariatur quos reiciendis odit at suscipit distinctio rem mollitia quo molestias eos expedita necessitatibus, fugiat molestiae quidem, nemo alias aliquid, corporis perspiciatis. Eveniet magnam rem expedita.
-      Itaque voluptatum perspiciatis libero quo? Temporibus voluptatibus consectetur alias tempore et iusto culpa, aspernatur nesciunt delectus iure impedit recusandae sit nihil nostrum perspiciatis debitis, veniam odit ullam quaerat! Vero, qui?
-      Omnis officia fugiat modi consequuntur. At temporibus quo est mollitia, adipisci officiis minima dolore error repellat facere hic doloribus sit odio totam dolorem saepe provident quia nostrum consequatur assumenda nulla.
-      Fugiat est culpa rem id quod aliquid voluptatum esse officia minima nostrum expedita, tempore ipsam reiciendis eius? Consequuntur voluptatum quas dolorem, in velit iure eaque? Perferendis dolor aperiam debitis tempore?
-      Odio inventore ullam tempore error, officiis voluptatibus, fugiat distinctio voluptatum nulla numquam temporibus iusto saepe ratione at earum dolorem, culpa ducimus animi quidem rem aliquid. Eius nam dolore quam autem!
-      Doloribus aut vel nobis eaque dolor. Impedit harum deserunt ratione deleniti consequuntur, alias magnam. Qui, adipisci beatae pariatur tempore quod culpa quas deleniti, soluta facilis voluptas placeat aut alias commodi?
-      Distinctio ipsum quaerat optio, reprehenderit beatae vero, dignissimos porro quos odio consequatur cum dolorem minus modi? Similique, sapiente nihil, aspernatur quae maxime unde quaerat, rerum perspiciatis debitis mollitia non eius.
-      Voluptate officiis incidunt animi quia dignissimos natus tempora quasi, soluta expedita tenetur illum cumque, perspiciatis rerum nam, mollitia ipsum vel sint non suscipit. Minus a eos, voluptas ea rem corporis?
-      Consectetur quisquam veniam esse tenetur illo inventore assumenda eveniet similique delectus ipsa! Repellat recusandae consectetur nobis voluptas quam dolores dolor dignissimos eos velit, sit minima eaque sunt ullam quae cumque.
-      Soluta voluptatum aut nobis natus maiores doloremque architecto facere est quisquam? Incidunt rerum nulla alias ea. At cum laudantium earum, ab libero repellendus suscipit accusantium est minus autem explicabo soluta?
-      Adipisci placeat minima iusto corrupti, aliquam veritatis eos ea sit pariatur sint, culpa ipsam suscipit labore rerum modi exercitationem asperiores aliquid magni ut corporis optio dolor deleniti. Voluptates, doloribus hic.
-      Quos quae assumenda porro et illo nulla, voluptas, nobis maxime laudantium, consequuntur dolores voluptates quam doloribus odit delectus! Consequatur fugiat deleniti doloribus dolorum eligendi ut ab nisi laudantium rerum non.
-      Est qui labore, eos cum non magni odit a animi dolorem, sapiente nemo fugiat dolore architecto maiores consequuntur ipsam minus vero quis quam corporis in commodi. Pariatur sunt veniam illum.
-      Reiciendis iusto architecto eligendi iste illo cupiditate nulla omnis, eius minima voluptatibus placeat deserunt odit velit itaque quod cum minus quibusdam et molestiae rem odio! Ut omnis excepturi in mollitia.
-      Expedita, voluptates quo? Consequuntur provident perspiciatis consectetur eveniet, accusantium fuga quibusdam eos quisquam aut aperiam nisi adipisci omnis ipsa fugiat, mollitia repudiandae eligendi explicabo ullam nemo cupiditate quos delectus vitae.
-      Quae, ullam quisquam magni sunt temporibus accusantium beatae libero laudantium vitae, suscipit modi itaque voluptatibus excepturi dignissimos dolor deleniti quibusdam pariatur ex. Vitae aut excepturi quod, atque recusandae quo magnam.
-      Sunt repellat labore delectus ipsum quia veniam! Ratione, molestiae, ipsum nemo odio distinctio unde minus numquam suscipit nam aut omnis eos non eius nisi. Sequi culpa ducimus neque vel quam!
-      Aliquid molestias voluptate eveniet illo dolor est sunt doloribus asperiores esse, voluptatibus sed, fugiat harum perferendis adipisci numquam saepe laborum et. Iusto nulla, illum totam consequuntur fuga ex quae! Deserunt!
-      Ex suscipit officiis iure harum magnam sit neque molestiae maxime maiores dolores voluptatum aperiam non reiciendis, ut quos corporis facilis. Ab iste expedita eveniet iusto iure nemo excepturi beatae error!
-      Ipsam enim mollitia eos unde, fuga ullam reiciendis iste! Natus illum fugiat animi rem impedit, voluptate quisquam architecto quaerat iure incidunt tempore aperiam et? Ipsa vitae dolorum excepturi vero qui.
-      Magni consequuntur facilis dolores commodi, tenetur modi provident, ducimus quaerat eligendi quae sapiente dicta, omnis animi atque error ipsa? Animi reiciendis maxime tempora iusto, obcaecati maiores hic dolorem ab facilis.
-      Recusandae qui accusantium quibusdam enim adipisci dicta, rerum reiciendis. Repudiandae corporis non delectus consectetur voluptatum dolorem error aliquam libero, vitae unde. Aspernatur, consequuntur dignissimos. Impedit sapiente saepe eos harum maxime.
-      Sint, tempora veritatis modi facere tempore molestiae minus cupiditate nulla rerum iusto libero quam. Eveniet, reiciendis voluptatibus! Atque earum dignissimos incidunt a voluptatem odio. Nulla aut iure similique totam fugiat.</p>
+
     </>
   )
 }
